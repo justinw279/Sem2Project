15d060b958dba62bbe01b15f3cb0b09fbf1e055e
BT_2N2_10 <law@bths.edu>
2024-03-15 13:06:16 -0400

    Commit

--- Class.java
+++ Class.java
@@ -1,2 +1,9 @@
 public class Class {
+
+    private String className;
+    private SeatingChart seatingChart;
+    public Class(String className, int numStudents, int numRows, int numColumns) {
+        seatingChart = new SeatingChart(numStudents, numRows, numColumns);
+        this.className = className;
+    }
 }
--- SeatingChart.java
+++ SeatingChart.java
@@ -1,4 +1,28 @@
 public class SeatingChart {
+    private Student[][] seatingChart;
+    public SeatingChart(int numStudents, int numRows, int numColumns) {
+        seatingChart = new Student[numRows][numColumns];
+        int counter = 0;
+        while (counter <= numStudents)
+        for(int i = 0; i < seatingChart.length; i++) {
+            for (int k = 0; k < seatingChart[i].length; k++) {
+                seatingChart[i][k] = new Student();
+                counter++;
+            }
+        }
+    }
+
+    public Student getStudent(String name) {
+        return new Student();
+    }
+
+    public Student getStudent(int id) {
+        return new Student();
+    }
+
+    public Student getStudent(int row, int column) {
+        return new Student();
+    }
 
 
 }
